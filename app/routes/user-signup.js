2f8b171079cfd9478d0a9fb4e258593b8b19fd7c
Gary Luo <weijialuo@yahoo-inc.com>
2017-11-10 20:03:50 -0600

    finish style

--- app/routes/user-signup.js
+++ app/routes/user-signup.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return Ember.Object.create({email: '', password: ''});
+  },
+  actions: {
+    signup(model) {
+      console.log(model)
+    }
+  }
 });
