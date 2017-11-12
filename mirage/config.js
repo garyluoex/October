f4468498b1ce2bc9ff7d01619da826a37125caa0
Gary Luo <weijialuo@yahoo-inc.com>
2017-11-12 01:13:58 -0600

    Add mirage and ember-browserfy

---
+++ mirage/config.js
@@ -0,0 +1,37 @@
+import JWT from "npm:jsonwebtoken";
+
+export default function() {
+
+
+  this.namespace = 'api';
+
+  this.post('/tokens', (schema, request) => {
+    return {
+        token: JWT.sign({email: request}, 'secretkey')
+    };
+  });
+
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Shorthand cheatsheet:
+
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+
+    http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
+  */
+}
