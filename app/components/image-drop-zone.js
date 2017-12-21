fb80dbd6787bc8cf4786dcc99aa181e5153b7a49
Gary Luo <weijialuo@yahoo-inc.com>
2017-12-21 12:27:10 -0800

    Start wroking on user droped photos

--- app/components/image-drop-zone.js
+++ app/components/image-drop-zone.js
@@ -1,25 +1,29 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  dragOver: function(event){
+  tagName: 'button',
+  classNames: ['image-drop-zone'],
+
+  dragOver: function(event) {
     // this is needed to avoid the default behaviour from the browser
     event.preventDefault();
   },
 
-  dragEnter: function(event){
+  dragEnter: function(event) {
     event.preventDefault();
   },
 
-  dragLeave: function(event){
+  dragLeave: function(event) {
     event.preventDefault();
   },
 
-  drop: function(event){
+  drop: function(event) {
     event.preventDefault();
-  },
-  actions: {
-    async uploadImage(model, file) {
-      console.log(await file.readAsDataURL());
-    }
+    var _this = this;
+    var reader = new FileReader();
+    reader.readAsDataURL(event.dataTransfer.files[0]);
+    reader.onload = function() {
+      _this.sendAction('uploadAction', reader.result);
+    };
   }
 });
